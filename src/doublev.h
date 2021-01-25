0490f023ea1157f47e8565f1e733b8963270e1af
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2021-01-25 16:42:19 +0100

    Add doublev.c doublev.h
    Finally doublev struct is going to be usefull

---
+++ src/doublev.h
@@ -0,0 +1,27 @@
+#ifndef DOUBLEV_H
+#define DOUBLEV_H
+
+#include<stdbool.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include <stdarg.h>
+
+// Vector of double
+struct doublev {
+    double *v;
+    size_t len;
+};
+
+bool eql_doublev(struct doublev *double_vector1, struct doublev *double_vector2);
+struct doublev *new_doublev(size_t len);
+struct doublev *new_doublev_and_fill(size_t len, ...);
+struct doublev *slice_doublev(struct doublev *double_vector, size_t low_index, size_t high_index);
+void append_doublev(struct doublev *double_vector, double to_append);
+void clear_doublev(struct doublev *double_vector);
+void concat_doublev(struct doublev *double_vector, struct doublev *to_add);
+void copy_doublev(struct doublev *to, struct doublev *from);
+void free_doublev(struct doublev *double_vector);
+void print_doublev(struct doublev *double_vector);
+
+#endif
