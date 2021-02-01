2d0e5e967c2cce7f97d2da976a1f0db166bba56d
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2021-02-01 21:59:24 +0100

    Clean

--- src/bst.h
+++ src/bst.h
@@ -23,14 +23,14 @@ struct bst {
 bool contains_bst(struct bst *b, double to_find);
 int height(struct bst *b);
 struct bst *new_bst(double key);
+struct bst *new_bst_and_fill(size_t nodes_count, ...);
+struct bst *new_empty_bst();
 struct bst *random_bst(size_t nodes);
 struct doublev *bst_to_doublev(struct bst *b);
 void append_bst(struct bst *b, double to_append);
 void bst_to_doublev_aux(struct bst *b, struct doublev *dv);
 void free_bst(struct bst *b);
 void print_bst(struct bst *b);
-struct bst *new_bst_and_fill(size_t nodes_count, ...);
-struct bst *new_empty_bst();
 
 void print_offset(FILE* stream, int offset);
 
